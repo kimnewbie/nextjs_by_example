27bd7c81f5e511edc7ef3e046334f91d47c8f6bd
youjinkim <gracecancho@myatman.com>
2023-03-02 14:51:03 +0900

    16.First Post Page

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Head from 'next/head';
+
 function AboutPage() {
     return (
         <>
+            <Head>About - My Blog</Head>
             <main>
                 <h1>About</h1>
             </main>
