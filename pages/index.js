0a27908253c268770a32c7bf385788cdcf640509
youjinkim <gracecancho@myatman.com>
2023-03-02 16:38:04 +0900

    27.Dynamic Routes in Production Mode

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ function HomePage({ posts }) {
         <>
             <Head>
                 <title>My Blog</title>
-                <meta name="description" content="This is my blog" />
             </Head>
             <main>
                 <h1>My Blog</h1>
