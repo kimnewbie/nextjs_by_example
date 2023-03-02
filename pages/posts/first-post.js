4793135e174df3b82daebd7735ea48f1b3ef7961
youjinkim <gracecancho@myatman.com>
2023-03-02 15:04:31 +0900

    18.getStaticProps Function

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,23 @@
 import Head from "next/head";
 
-function FirstPostPage() {
+export async function getStaticProps() {
+  return {
+    props: {
+      post: {
+        title: 'Fist Post',
+        body: 'My first post, as static props'
+      }
+    }
+  }
+}
+
+function FirstPostPage({ post }) {
   return (
     <>
-      <Head>First Post - My Blog</Head>
+      <Head>{post.title} - My Blog</Head>
       <main>
-        <h1>First Post</h1>
-        <p>This is my first ever blog post!</p>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
       </main>
     </>
   );
