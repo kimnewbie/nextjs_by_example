856cf8663ecc1f0a36a489fdf1f373ee9dbe3fd1
youjin kim <gracecancho@myatman.com>
2023-05-09 10:32:46 +0900

    test

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+function AboutPage() {
+    // test
+    return (
+        <>
+            <main>
+                <h1>About</h1>
+            </main>
+        </>
+    )
+}
+
+export default AboutPage;
