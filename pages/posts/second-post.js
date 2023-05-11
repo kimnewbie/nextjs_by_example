8b57aeacfefac19bcf02d22db2edaca3e358098d
youjin kim <gracecancho@gmail.com>
2023-05-11 09:41:14 +0900

    test

--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,28 +1,28 @@
-import Head from "next/head";
-import { getPost } from "../../lib/posts";
+// import Head from "next/head";
+// import { getPost } from "../../lib/posts";
 
-/* Props를 넘기는 Nextjs의 방법 */
-export async function getStaticProps() {
-    const post = await getPost('second-post'); // slug
-    return {
-        props: { post },
-    };
-};
+// /* Props를 넘기는 Nextjs의 방법 */
+// export async function getStaticProps() {
+//     const post = await getPost('second-post'); // slug
+//     return {
+//         props: { post },
+//     };
+// };
 
-function SecondPostPage({ post }) {
-    console.log('[SecondPostPage] render:', post)
-    return (
-        <>
-            <Head>
-                <title>{post.title} - My Blog</title>
-            </Head>
-            <main>
-                <p>{post.date}</p>
-                <h1>{post.title}</h1>
-                <article dangerouslySetInnerHTML={{ __html: post.body }}></article>
-            </main>
-        </>
-    )
-}
+// function SecondPostPage({ post }) {
+//     console.log('[SecondPostPage] render:', post)
+//     return (
+//         <>
+//             <Head>
+//                 <title>{post.title} - My Blog</title>
+//             </Head>
+//             <main>
+//                 <p>{post.date}</p>
+//                 <h1>{post.title}</h1>
+//                 <article dangerouslySetInnerHTML={{ __html: post.body }}></article>
+//             </main>
+//         </>
+//     )
+// }
 
-export default SecondPostPage;
+// export default SecondPostPage;
